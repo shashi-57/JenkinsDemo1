aeb48d547f935080fbed28c482f5080b4f7cbe98
shashi-57 <245122748057@mvsrec.edu.in>
2025-03-18 12:32:06 +0530

    Update add.java

--- add.java
+++ add.java
@@ -3,8 +3,8 @@ public class add
 {
     public static void main(String[] args)
     {
-        double num1 = 13;  // Predefined first number
-        double num2 = 18;  // Predefined second number
+        double num1 = 13;  
+        double num2 = 18;  
 
         double sum = num1 + num2;
         System.out.println("The sum is: " + sum);

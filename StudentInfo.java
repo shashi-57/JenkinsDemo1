d8bfc8b36d2c6e92f57d2f31fd6fbc3cbfdcadd9
shashi-57 <245122748057@mvsrec.edu.in>
2025-03-24 12:58:37 +0530

    Add files via upload

---
+++ StudentInfo.java
@@ -0,0 +1,13 @@
+class StudentInfo
+{
+public static void main(String[] args)
+{
+String s1=args[0];
+String s2=args[1];
+String s3=args[2];
+System.out.println("Name: " +s1);
+System.out.println("Roll Number: " +s2);
+System.out.println("Department: " +s3);
+}
+}
+
